0f0554382b512b9e0eb24a6b37e8d5d428dc4bf7
João Aguiar <jocm.aguiar@gmail.com>
2023-07-12 15:59:10 -0300

    task : update content

--- xios/aid/responseheader.py
+++ xios/aid/responseheader.py
@@ -13,32 +13,33 @@ class ResponseHeader( object ):
         self.v_status = v_status
         self.v_headers = v_headers
 
-    # Sysntax Objective : Define Method : Status Code : INTERNAL_SERVER_ERROR : OK : FORMAT : Text
-    def ok_text( self, v_resp ):
-
-        self.v_value = str( HTTPStatus.OK.value )
-        self.v_phrase = str( HTTPStatus.OK.phrase )
-        self.v_status = self.v_value + ' ' + self.v_phrase
-        self.v_headers = [('Content-type', 'text/plain; charset=utf-8')]
-
-        return( v_resp(self.v_status, self.v_headers) )
-
     # Sysntax Objective : Define Method : Status Code : INTERNAL_SERVER_ERROR : OK : FORMAT : Html
-    def ok_html( self, v_resp ):
-
+    def http_200( self, v_resp ):
         self.v_value = str( HTTPStatus.OK.value )
         self.v_phrase = str( HTTPStatus.OK.phrase )
         self.v_status = self.v_value + ' ' + self.v_phrase
         self.v_headers = [( 'Content-type', 'text/html; charset=utf-8' )]
-
         return( v_resp(self.v_status, self.v_headers) )
+    
+    # Sysntax Objective : Define Method : Status Code : UNAUTHORIZED : UNAUTHORIZED : FORMAT : Text
+    def http_401( self, v_resp ):
+
+        self.v_value = str( HTTPStatus.UNAUTHORIZED.value )
+        self.v_phrase = str( HTTPStatus.UNAUTHORIZED.phrase )
+        self.v_status = self.v_value + ' ' + self.v_phrase
+        self.v_headers = [
+            ( 'Content-type', 'text/plain; charset=utf-8' ),
+            ( 'WWW-Authenticate', 'Basic realm=Private Acess' )
+        ]
+        v_resp(self.v_status, self.v_headers)
+        return ( 'Error: 401 Unauthorized.' )
 
     # Sysntax Objective : Define Method : Status Code : INTERNAL_SERVER_ERROR : INTERNAL_SERVER_ERROR : FORMAT : Text
-    def ise_text( self, v_resp ):
+    def http_500( self, v_resp ):
 
         self.v_value = str( HTTPStatus.INTERNAL_SERVER_ERROR.value )
         self.v_phrase = str( HTTPStatus.INTERNAL_SERVER_ERROR.phrase )
         self.v_status = self.v_value + ' ' + self.v_phrase
         self.v_headers = [( 'Content-type', 'text/plain; charset=utf-8' )]
         v_resp(self.v_status, self.v_headers)
-        return [str( 'Error: 500 Internal Server Error.' ).encode( 'utf-8' )]
+        return ( 'Error: 500 Internal Server Error.' )
